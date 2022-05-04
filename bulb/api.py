4ffa616fe845a6cf3337e89d0dee93a3b300bcdc
Artem <burenotti@gmail.com>
2022-05-04 23:41:06 +0300

    ✨ Added stats endpoints that returns available languages and versions

--- bulb/api.py
+++ bulb/api.py
@@ -1,21 +1,34 @@
-from fastapi import APIRouter, WebSocket
+import itertools
 
 from fastapi import APIRouter, WebSocket
 
 from . import services
+from .cfg import config
 from .exceptions import MissingProfileError, BuildFailedError
-from .models import MissingProfileMessage, BuildFailedMessage, FinishMessage
+from .models import Stats, Language, MissingProfileMessage, BuildFailedMessage, FinishMessage
 from .services import send_output_ws, get_input_ws
 
 router = APIRouter(prefix='/code')
 
 
+@router.get('/stats', response_model=Stats)
+async def get_stats() -> Stats:
+    langs = itertools.groupby(config.languages, lambda lang: lang.language)
+
+    return Stats(available_languages=[
+        Language(
+            language=name,
+            versions=[lang.version for lang in group],
+        ) for name, group in langs
+    ])
+
+
 @router.websocket('/run')
 async def run_code(
-    ws: WebSocket,
-    language: str,
-    code: str,
-    version: str | None = None,
+        ws: WebSocket,
+        language: str,
+        code: str,
+        version: str | None = None,
 ):
     await ws.accept()
     try:
