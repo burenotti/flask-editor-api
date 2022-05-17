1b9b6a2e8f5827dc440cee866e9ee8a89ed21d14
Artem <burenotti@gmail.com>
2022-05-17 13:23:21 +0300

    ✨ Added pipeline factory

---
+++ bulb/pipeline_factory.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Callable
+
+from bulb.cfg import LanguageProfile
+from bulb.exceptions import MissingProfileError
+from bulb.proto import ExecutionPipeline
+
+PipelineOrCallable = ExecutionPipeline | Callable[[LanguageProfile], ExecutionPipeline]
+
+
+@dataclass
+class Route:
+    lang: LanguageProfile
+    raw_pipeline: PipelineOrCallable
+
+    @property
+    def pipeline(self) -> ExecutionPipeline:
+        if callable(self.raw_pipeline):
+            return self.raw_pipeline(self.lang)
+        else:
+            return self.raw_pipeline
+
+
+class PipelineFactory:
+
+    def __init__(self):
+        self.routes: list[Route] = []
+
+    def register(self, lang: LanguageProfile, pipeline: PipelineOrCallable) -> PipelineFactory:
+        self.routes.append(Route(lang, pipeline))
+        return self
+
+    def _get_route(self, lang: str, lang_ver: str) -> Route:
+        filtered = [
+            route for route in self.routes
+            if route.lang.language == lang and
+               (lang_ver is None or lang_ver == route.lang.version)
+        ]
+
+        if not filtered:
+            raise MissingProfileError(lang, lang_ver)
+
+        return filtered[0]
+
+    def create_pipeline(self, lang: str, lang_ver: str) -> ExecutionPipeline:
+        route = self._get_route(lang, lang_ver)
+        return route.pipeline
