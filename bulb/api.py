78252984451fa13d64e47dc6656e88fe3be77e0e
Artem <burenotti@gmail.com>
2022-05-17 13:25:29 +0300

    ✨🔧 Added loader.py file
    loader.py is a temporary solution to store language profile
    configuration and register them in pipeline factory

--- bulb/api.py
+++ bulb/api.py
@@ -1,12 +1,10 @@
 import itertools
 
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, Depends
 
-from . import services
 from .cfg import config
-from .exceptions import MissingProfileError, BuildFailedError
-from .models import Stats, Language, MissingProfileMessage, BuildFailedMessage, FinishMessage
-from .services import send_output_ws, get_input_ws
+from .models import Stats, Language
+from .services import RunboxService
 
 router = APIRouter(prefix='/code')
 
@@ -25,23 +23,11 @@ async def get_stats() -> Stats:
 
 @router.websocket('/run')
 async def run_code(
-        ws: WebSocket,
-        language: str,
-        code: str,
-        version: str | None = None,
+    ws: WebSocket,
+    language: str,
+    code: str,
+    version: str | None = None,
+    service: RunboxService = Depends(),
 ):
     await ws.accept()
-    try:
-        state = await services.run_code(
-            language, code,
-            version=version,
-            get_input=get_input_ws(ws),
-            on_output=send_output_ws(ws),
-        )
-
-        await ws.send_text(FinishMessage(**state.dict(by_alias=True)).json())
-
-    except BuildFailedError as why:
-        await ws.send_text(BuildFailedMessage(logs=why.logs).json())
-    except MissingProfileError:
-        await ws.send_text(MissingProfileMessage().json())
+    await service.run_code(language, version, code)
