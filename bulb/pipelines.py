d7d953e5bbf784b56f9e39a1187b3e1e64a51aea
Artem <burenotti@gmail.com>
2022-05-17 13:23:39 +0300

    ✨ Added default pipeline generators

---
+++ bulb/pipelines.py
@@ -0,0 +1,25 @@
+from .base_execution_pipeline import BaseExecutionPipeline
+from .build_stage import *
+from .cfg import LanguageProfile
+
+
+def get_default_non_building_pipeline(profile: LanguageProfile) -> BaseExecutionPipeline:
+    pipeline = BaseExecutionPipeline() \
+        .with_profile(profile) \
+        .then(CreateSandbox("sandbox")) \
+        .then(WriteSourceCode("/sandbox", "sandbox")) \
+        .then(Run("sandbox")) \
+        .then(Observe("sandbox"))
+
+    return pipeline
+
+
+def get_pipeline_with_default_build(profile: LanguageProfile) -> BaseExecutionPipeline:
+    pipeline = BaseExecutionPipeline() \
+        .with_profile(profile) \
+        .then(BuildAndMountAt('/build')) \
+        .then(CreateSandbox("sandbox")) \
+        .then(Run("sandbox")) \
+        .then(Observe("sandbox"))
+
+    return pipeline
