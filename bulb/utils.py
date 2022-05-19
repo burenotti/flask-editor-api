a96c2349048d4750e9b9fbebb49df11959f2b79d
Artem <burenotti@gmail.com>
2022-05-19 23:01:40 +0300

    ✨ Added @once_init_async

--- bulb/utils.py
+++ bulb/utils.py
@@ -19,3 +19,19 @@ def once_init(function: Callable[T_args, T_ret]) -> Callable[T_args, T_ret]:
         return cache
 
     return wrapper
+
+
+def once_init_async(function: Callable[T_args, T_ret]) -> Callable[T_args, T_ret]:
+    cache = None
+    inited = False
+
+    @functools.wraps(function)
+    async def wrapper(*args: T_args.args, **kwargs: T_args.kwargs) -> T_ret:
+        nonlocal inited, cache
+        if not inited:
+            cache = await function(*args, **kwargs)
+        inited = True
+
+        return cache
+
+    return wrapper
