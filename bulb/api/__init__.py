3bbd2831e9a19ef2baefec15b72496566875d41a
Artem <burenotti@gmail.com>
2022-05-19 23:12:21 +0300

    ✨ Added github OAuth

--- bulb/api/__init__.py
+++ bulb/api/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from . import execution
+from . import auth
 
 router = APIRouter()
 
 router.include_router(execution.router)
+router.include_router(auth.router, prefix="/oauth")
--- bulb/api/auth.py
+++ bulb/api/auth.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
 from ..cfg import config
+from ..services import RedirectGithubOAuth
 
 router = APIRouter()
 
-github_oauth = GithubOAuth(config.github, tags=['Github Authentication'])
+github_oauth = RedirectGithubOAuth(config.github, tags=['Github Authentication'])
 router.include_router(github_oauth.router, prefix="/github")
