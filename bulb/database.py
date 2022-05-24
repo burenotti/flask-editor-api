49e580c562ff5d38186377f357c821bfed0e131b
Artem <burenotti@gmail.com>
2022-05-24 16:09:50 +0300

    🐛 Fixed get_session

--- bulb/database.py
+++ bulb/database.py
@@ -7,9 +7,9 @@ from .cfg import config
 
 async_engine = create_async_engine(config.db_dsn, echo=config.debug, future=True)
 async_session = sessionmaker(async_engine, class_=AsyncSession,
-                             expire_on_commit=False, autocommit=True)
+                             expire_on_commit=False, autocommit=False)
 
 
 async def get_session():
-    async with async_session as session:
+    async with async_session() as session:
         yield session
